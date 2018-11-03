9ad1db26fef279c36e0927c7b6e6a87b56d60ff9
Hong Tang <dremofly@gmail.com>
2018-11-03 21:40:31 +0800

    The part3 version

--- block.py
+++ block.py
@@ -1,20 +1,27 @@
 #encoding:utf-8
 import time
 import hashlib
+import proofofwork as pfw
 
 class Block(object):
     def __init__(self, data, prevBlockHash):
         self.Timestamp = time.time()
         self.Data = data
         self.PrevBlockHash = prevBlockHash  #string
-        self.Hash = self.SetHash()
+        self.Nonce = 0
+        self.Hash = ''
+        self.Validate = False
+        self.SetHash()
     
     def SetHash(self):  #TODO:builtin
-        encoder = hashlib.sha256()
-        header = self.PrevBlockHash + self.Data + str(self.Timestamp)
-        encoder.update(header.encode("utf-8"))
-        return encoder.hexdigest()
-    
+        proofofwork = pfw.ProofOfWork(self)
+        nonce, Hash = proofofwork.Run()
+        self.Hash = Hash
+        self.Nonce = nonce
+        self.Validate = proofofwork.Validate()
+           
     def __str__(self):
-        return "PrevBlockHash: " + self.PrevBlockHash + "\n" + "Data: " + self.Data + "\n" + "Hash: " + self.Hash + "\n"
+        return "PrevBlockHash: " + self.PrevBlockHash + "\n" + "Data: " + self.Data + "\n" + "Hash: " + self.Hash + "\n" + "Nonce: " + str(self.Nonce) + "\n" + "Validate: " + str(self.Validate) + "\n"
+    
+    
 
--- main.py
+++ main.py
@@ -2,17 +2,16 @@
 import hashlib
 import block as b 
 import blockchain as bc
+import proofofwork as pfw
 
 blockchain = bc.Blockchain()
 blockchain.NewBlockchain()
-blockchain.AddBlock("Send 1 BTC to Hong")
+err = blockchain.AddBlock("Send 1 BTC to Hong")
+if err:
+    print("[Warning!] Failed to add block \"%s\""%("Send 1 BTC to Hong"))
 blockchain.AddBlock("Send 2 BTC to Hong")
-
-#for block in blockchain.blocks:
-#    print("Prevhash: ", block.PrevBlockHash)
-#    print("Data: ", block.Data)
-#    print("Hash: ", block.Hash)
-#    print("\n")
+if err:
+    print("[Warning!] Failed to add block \"%s\""%("Send 2 BTC to Hong"))
 
 for block in blockchain.blocks:
     print(block)
