af955f3847c13cc1f3050ca22494fc942b388653
Hong Tang <dremofly@gmail.com>
2018-11-06 20:22:35 +0800

    add serilize and deserialize method

--- block.py
+++ block.py
@@ -4,7 +4,7 @@ import hashlib
 import proofofwork as pfw
 
 class Block(object):
-    def __init__(self, data, prevBlockHash):
+    def __init__(self, data='', prevBlockHash=''):
         self.Timestamp = time.time()
         self.Data = data
         self.PrevBlockHash = prevBlockHash  #string
@@ -23,5 +23,14 @@ class Block(object):
     def __str__(self):
         return "PrevBlockHash: " + self.PrevBlockHash + "\n" + "Data: " + self.Data + "\n" + "Hash: " + self.Hash + "\n" + "Nonce: " + str(self.Nonce) + "\n" + "Validate: " + str(self.Validate) + "\n"
     
+    def Serialize(self):
+        return [self.Timestamp, self.Data, self.PrevBlockHash, self.Nonce, self.Hash, self.Validate]
+    def Deserialize(self, data):
+        self.Timestamp = data[0]
+        self.Data = data[1]
+        self.PrevBlockHash = data[2]
+        self.Nonce = data[3]
+        self.Hash = data[4]
+        self.Validate = data[5]
     
 
